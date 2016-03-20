573f1acfa3dc428a16ba4eb445cd7698afe28a64
Cristian Dinu <goc9000@gmail.com>
2016-03-20 00:34:42 +0000

    Test harness: iterate through Yahoo test archives

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <QDebug>
+#include <QDirIterator>
 
 
 int main() {
-    qDebug() << "test";
+    // Temporary test harness
+
+    QDirIterator yahoo_files(
+            QT_STRINGIFY(TEST_DATA_DIR) "/yahoo",
+            QStringList() << "*.dat",
+            QDir::Files,
+            QDirIterator::Subdirectories
+    );
+
+    while (yahoo_files.hasNext()) {
+        qDebug() << yahoo_files.next();
+    }
 
     return 0;
 }
