9516e9ddfb7505c9ee16b726d30652aa9771a268
Cristian Dinu <goc9000@gmail.com>
2016-04-24 23:26:52 +0100

    Added "uninterpreted" event type
    This is a sort of placeholder event to enable incrementally implementing parsing various event types.

---
+++ src/intermediate_format/events/IFUninterpretedEvent.cpp
@@ -0,0 +1,32 @@
+/**
+ * intermediate_format/events/IFUninterpretedEvent.cpp
+ *
+ * (C) Copyright 2014-present  Cristian Dinu <goc9000@gmail.com>
+ *
+ * This file is part of UniArchive II.
+ *
+ * Licensed under the GPL-3
+ */
+
+#include <QDebugStateSaver>
+
+#include "intermediate_format/events/IFUninterpretedEvent.h"
+
+namespace uniarchive2 { namespace intermediate_format { namespace events {
+
+IFUninterpretedEvent::IFUninterpretedEvent(
+    ApparentTime timestamp,
+    unsigned int index,
+    const QByteArray& raw_data
+): IntermediateFormatEvent(timestamp, index), rawData(raw_data) {
+}
+
+QString IFUninterpretedEvent::eventName() const {
+    return "Uninterpreted";
+}
+
+void IFUninterpretedEvent::writeDetailsToDebugStream(QDebug stream) const {
+    stream << " data=" << rawData;
+}
+
+}}}
---
+++ src/intermediate_format/events/IFUninterpretedEvent.h
@@ -0,0 +1,39 @@
+/**
+ * intermediate_format/events/IFUninterpretedEvent.h
+ *
+ * (C) Copyright 2014-present  Cristian Dinu <goc9000@gmail.com>
+ *
+ * This file is part of UniArchive II.
+ *
+ * Licensed under the GPL-3
+ */
+
+#ifndef UNIARCHIVE2_INTERMEDIATE_FORMAT_EVENTS_IFUNINTERPRETEDEVENT_H
+#define UNIARCHIVE2_INTERMEDIATE_FORMAT_EVENTS_IFUNINTERPRETEDEVENT_H
+
+#include <memory>
+
+#include <QtDebug>
+#include <QByteArray>
+
+#include "intermediate_format/events/IntermediateFormatEvent.h"
+
+using namespace std;
+
+namespace uniarchive2 { namespace intermediate_format { namespace events {
+
+struct IFUninterpretedEvent : IntermediateFormatEvent {
+public:
+    QByteArray rawData;
+
+    IFUninterpretedEvent(ApparentTime timestamp, unsigned int index, const QByteArray& raw_data);
+
+    virtual QString eventName() const;
+
+protected:
+    virtual void writeDetailsToDebugStream(QDebug stream) const;
+};
+
+}}}
+
+#endif //UNIARCHIVE2_INTERMEDIATE_FORMAT_EVENTS_IFUNINTERPRETEDEVENT_H
