814a3761a468076daef6309a80552f088010fa1c
Cristian Dinu <goc9000@gmail.com>
2016-04-30 03:14:54 +0100

    Added shortcut IMM(T) for const T&

---
+++ src/utils/language/shortcuts.h
@@ -0,0 +1,16 @@
+/**
+ * utils/language/shortcuts.h
+ *
+ * (C) Copyright 2014-present  Cristian Dinu <goc9000@gmail.com>
+ *
+ * This file is part of UniArchive II.
+ *
+ * Licensed under the GPL-3
+ */
+
+#ifndef UNIARCHIVE2_UTILS_LANGUAGE_SHORTCUTS_H
+#define UNIARCHIVE2_UTILS_LANGUAGE_SHORTCUTS_H
+
+#define IMM(type) const type&
+
+#endif //UNIARCHIVE2_UTILS_LANGUAGE_SHORTCUTS_H
