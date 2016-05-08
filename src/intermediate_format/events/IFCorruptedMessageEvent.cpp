5f2e439bb97a4b443199d4031c2c81cd09933322
Cristian Dinu <goc9000@gmail.com>
2016-05-08 21:19:53 +0100

    Added message type for corrupted message data

---
+++ src/intermediate_format/events/IFCorruptedMessageEvent.cpp
@@ -0,0 +1,32 @@
+/**
+ * intermediate_format/events/IFCorruptedMessageEvent.cpp
+ *
+ * (C) Copyright 2014-present  Cristian Dinu <goc9000@gmail.com>
+ *
+ * This file is part of UniArchive II.
+ *
+ * Licensed under the GPL-3
+ */
+
+#include <QDebugStateSaver>
+
+#include "intermediate_format/events/IFCorruptedMessageEvent.h"
+
+namespace uniarchive2 { namespace intermediate_format { namespace events {
+
+IFCorruptedMessageEvent::IFCorruptedMessageEvent(
+    IMM(ApparentTime) timestamp,
+    unsigned int index,
+    IMM(QByteArray) raw_data
+) : IntermediateFormatEvent(timestamp, index), rawData(raw_data) {
+}
+
+QString IFCorruptedMessageEvent::eventName() const {
+    return "CorruptedMessage";
+}
+
+void IFCorruptedMessageEvent::writeDetailsToDebugStream(QDebug stream) const {
+    stream << " data=" << rawData;
+}
+
+}}}
