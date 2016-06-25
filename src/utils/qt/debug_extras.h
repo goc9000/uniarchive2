a0af3105c1d4c2b655441ef7df0e0a925c973041
Cristian Dinu <goc9000@gmail.com>
2016-06-25 03:35:58 +0100

    Added QtDebug printers for standard collections

---
+++ src/utils/qt/debug_extras.h
@@ -0,0 +1,97 @@
+/**
+ * utils/qt/debug_extras.h
+ *
+ * (C) Copyright 2014-present  Cristian Dinu <goc9000@gmail.com>
+ *
+ * This file is part of UniArchive II.
+ *
+ * Licensed under the GPL-3
+ */
+
+#ifndef UNIARCHIVE2_UTILS_QT_DEBUG_EXTRAS_H
+#define UNIARCHIVE2_UTILS_QT_DEBUG_EXTRAS_H
+
+#include "utils/language/shortcuts.h"
+
+#include <QtDebug>
+
+#include <vector>
+#include <unordered_set>
+#include <set>
+#include <map>
+
+template<typename T>
+QDebug operator<< (QDebug stream, IMM(std::vector<T>) value) {
+    QDebugStateSaver saver(stream);
+    stream.nospace() << "[";
+
+    if (!value.empty()) {
+        char const * sep = " ";
+
+        for (IMM(T) item : value) {
+            stream << sep << item;
+            sep = ", ";
+        }
+        stream << " ";
+    }
+
+    stream << "]";
+
+    return stream;
+}
+
+template<typename T>
+QDebug operator<< (QDebug stream, IMM(std::unordered_set<T>) value) {
+    QDebugStateSaver saver(stream);
+    stream.nospace() << "{";
+
+    if (!value.empty()) {
+        char const * sep = " ";
+
+        for (IMM(T) item : value) {
+            stream << sep << item;
+            sep = ", ";
+        }
+        stream << " ";
+    }
+
+    stream << "}";
+
+    return stream;
+}
+
+template<typename T>
+QDebug operator<< (QDebug stream, IMM(std::set<T>) value) {
+    QDebugStateSaver saver(stream);
+    stream.nospace() << "{";
+
+    if (!value.empty()) {
+        char const * sep = " ";
+
+        for (IMM(T) item : value) {
+            stream << sep << item;
+            sep = ", ";
+        }
+        stream << " ";
+    }
+
+    stream << "}";
+
+    return stream;
+}
+
+template<typename K, typename V>
+QDebug operator<< (QDebug stream, const std::map<K, V>& value) {
+    QDebugStateSaver saver(stream);
+    stream.nospace() << "{\n";
+
+    for (IMM(auto) kv : value) {
+        stream << "\t" << kv.first << " : " << kv.second << "\n";
+    }
+
+    stream << "}";
+
+    return stream;
+}
+
+#endif //UNIARCHIVE2_UTILS_QT_DEBUG_EXTRAS_H
