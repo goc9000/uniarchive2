35660c695e6d03ba5c2bce33d6d87f21699151e3
Cristian Dinu <goc9000@gmail.com>
2016-08-23 00:29:16 +0100

    Moved type registry to own class

---
+++ build_assistant/autogen_common.py
@@ -0,0 +1,14 @@
+# build_assistant/autogen_common.py
+#
+# (C) Copyright 2014-present  Cristian Dinu <goc9000@gmail.com>
+#
+# This file is part of Uniarchive II.
+#
+# Licensed under the GPL-3
+
+
+from build_assistant.VirtualPath import VirtualPath
+
+
+def get_full_autogen_raw_event_path_and_name(path, name):
+    return VirtualPath(['intermediate_format', 'events']).append(path), 'Raw' + name + 'Event'
