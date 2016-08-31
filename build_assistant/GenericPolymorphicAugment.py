5f553ed440c4526dc8aed03414ac7b387237c174
Cristian Dinu <goc9000@gmail.com>
2016-08-31 23:30:38 +0100

    Factored out more common polymorphics code

---
+++ build_assistant/GenericPolymorphicAugment.py
@@ -0,0 +1,129 @@
+# build_assistant/GenericPolymorphicAugment.py
+#
+# (C) Copyright 2014-present  Cristian Dinu <goc9000@gmail.com>
+#
+# This file is part of Uniarchive II.
+#
+# Licensed under the GPL-3
+
+from collections import namedtuple
+
+from build_assistant.Augment import Augment
+from build_assistant.AutoGenConfig import GenericPolymorphicConfig
+
+
+ConstructorInfo = namedtuple('ConstructorInfo', ['params', 'subconstructors', 'init_statements'])
+
+
+class GenericPolymorphicAugment(Augment):
+    _core = None
+
+    def __init__(self, poly_config, autogen_core, field_augment=None):
+        if field_augment is not None:
+            poly_config = poly_config._replace(
+                fields=[field_augment(field, autogen_core) for field in poly_config.fields]
+            )
+
+        Augment.__init__(self, poly_config, GenericPolymorphicConfig)
+
+        self._core = autogen_core
+
+    def mandatory_base_fields(self):
+        raise NotImplementedError
+
+    def class_name(self):
+        raise NotImplementedError
+
+    def parent_class(self, no_template=None):
+        raise NotImplementedError
+
+    def constructors(self):
+        base_fields = self.mandatory_base_fields()
+        free_fields = self.fields
+        parent_constructor = \
+            [self.parent_class(no_template=True) + '(' + ', '.join(f.as_rvalue() for f in base_fields) + ')'] \
+            if self.parent_class() is not None else []
+
+        maybe_addable_fields = filter(lambda f: f.add_to_constructor, free_fields)
+        extra_enabled_fields = set()
+
+        while True:
+            inited_fields = [f for f in free_fields if f.name in extra_enabled_fields or f.is_mandatory()]
+
+            yield ConstructorInfo(
+                params=[f.as_param() for f in base_fields + inited_fields],
+                subconstructors=parent_constructor + [f.as_subconstructor() for f in inited_fields],
+                init_statements=list(),
+            )
+
+            # Generate convenience constructor for the first singularizable field
+            for index, field_config in enumerate(inited_fields):
+                if field_config.maybe_singleton:
+                    singularized = field_config.singularized()
+
+                    params = \
+                        [f.as_param() for f in base_fields + inited_fields[:index]] + \
+                        [singularized.as_param()] + \
+                        [f.as_param() for f in inited_fields[index + 1:]]
+
+                    subcons = parent_constructor + \
+                              [f.as_subconstructor() for f in inited_fields[:index]] + \
+                              [f.as_subconstructor() for f in inited_fields[index + 1:]]
+
+                    yield ConstructorInfo(
+                        params=params,
+                        subconstructors=subcons,
+                        init_statements=[
+                            '{0}.push_back({1});'.format(field_config.name, singularized.as_rvalue())
+                        ],
+                    )
+                    break
+
+            field_config = next(maybe_addable_fields, None)
+            if field_config is None:
+                break
+
+            extra_enabled_fields.add(field_config.name)
+
+    def gen_field_declarations(self, block):
+        if len(self.fields) > 0:
+            for index, field in enumerate(self.fields):
+                if index in self.field_breaks:
+                    block.nl()
+
+                if field.doc is not None:
+                    block.doc_comment(field.doc)
+
+                block.field(*field.as_field_decl())
+
+            block.nl()
+
+    def gen_constructors(self, cpp_source):
+        for ctor_info in self.constructors():
+            with cpp_source.constructor(
+                 self.class_name(), *ctor_info.params, inherits=ctor_info.subconstructors, declare=True
+            ) as cons:
+                for line in ctor_info.init_statements:
+                    cons.line(line)
+
+                if self.has_mandatory_fields_sanity_check():
+                    cons.line('sanityCheckMandatoryParameters();')
+
+    def gen_mandatory_fields_sanity_check_method(self, cpp_source):
+        if self.has_mandatory_fields_sanity_check():
+            with cpp_source.method(self.class_name(), 'sanityCheckMandatoryParameters', 'void', const=True) as method:
+                for field in self.fields:
+                    if field.is_mandatory() and field.is_checkable():
+                        field.gen_param_check(method)
+
+    def gen_private_block(self, struct):
+        if self.has_private_block():
+            struct.nl()
+            with struct.private_block() as block:
+                block.declare_fn('sanityCheckMandatoryParameters', 'void', const=True)
+
+    def has_private_block(self):
+        return self.has_mandatory_fields_sanity_check()
+
+    def has_mandatory_fields_sanity_check(self):
+        return any(f.is_mandatory() and f.is_checkable() for f in self.fields)
