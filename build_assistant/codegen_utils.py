7c5dc0df928c2e6fa9b8dbc9f441ae7b3ff6286c
Cristian Dinu <goc9000@gmail.com>
2016-10-03 13:29:10 +0300

    Moved line filtering function to codegen_utils

--- build_assistant/codegen_utils.py
+++ build_assistant/codegen_utils.py
@@ -11,3 +11,16 @@ import json
 
 def cpp_string_literal(text):
     return json.dumps(text)  # Eh, probably works ok in most situations
+
+
+def filter_lines(lines):
+    """Collapses multiple empty lines into one, and filters out trailing empty lines"""
+    has_empty_line = False
+    for line in lines:
+        if line != '':
+            if has_empty_line:
+                yield ''
+                has_empty_line = False
+            yield line
+        else:
+            has_empty_line = True
