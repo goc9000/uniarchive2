b4f90e410a7e54263c38236fa0333dfd56e18ac3
Cristian Dinu <goc9000@gmail.com>
2016-10-23 20:00:10 +0300

    Clean up enum generation code

--- build_assistant/autogen_enums.py
+++ build_assistant/autogen_enums.py
@@ -31,11 +31,6 @@ def synthetic_enums(autogen_config):
 
 def gen_enums(autogen_config, autogen_core):
     for path, name, enum_config in all_enums(autogen_config):
-        varname = classname_to_varname(name)
-        parameter_spec = (name, varname)
-
-        name_for_function = 'name_for_' + camelcase_to_underscore(name)
-
         cpp_source, h_source = autogen_core.new_pair(path, name)
 
         if enum_config.internal_comment is not None:
@@ -55,30 +50,27 @@ def gen_enums(autogen_config, autogen_core):
 
                 block.line(line)
 
-        cpp_source.qt_include('QtDebug')
+        varname = classname_to_varname(name)
+        parameter_spec = (name, varname)
+        name_for_function = 'name_for_' + camelcase_to_underscore(name)
 
         with cpp_source.code.function(name_for_function, 'QString', parameter_spec, declare_in=h_source.code) as fn:
             with fn.switch_block(varname) as sw:
                 for value in enum_config.values:
-                    with sw.case_block(name + '::' + value.constant) as c:
-                        c.ret('{0}', cpp_string_literal(value.text))
+                    sw.case_block(name + '::' + value.constant).ret('{0}', cpp_string_literal(value.text))
 
             fn.code_line('invariant_violation("Invalid {0} value (%d)", {1})', name, varname)
 
         h_source.code.nl()
 
-        with cpp_source.code.function(
+        cpp_source.qt_include('QtDebug').use_symbol('QP').code.function(
             'operator<< ', 'QDebug', ('QDebug', 'stream'), parameter_spec, declare_in=h_source.code
-        ) as fn:
-            fn.code_line('stream << QP({0}({1}))', name_for_function, varname) \
-              .nl() \
-              .ret('stream')
+        ).code_line('stream << QP({0}({1}))', name_for_function, varname) \
+            .nl() \
+            .ret('stream')
 
-            cpp_source.use_symbol('QP')
-
-        with cpp_source.code.function(
+        cpp_source.code.function(
             'operator<< ', 'QDataStream&', ('QDataStream&', 'mut_stream'), parameter_spec, declare_in=h_source.code
-        ) as fn:
-            fn.code_line('mut_stream << (quint32){0}', varname) \
-                .nl() \
-                .ret('mut_stream')
+        ).code_line('mut_stream << (quint32){0}', varname) \
+            .nl() \
+            .ret('mut_stream')
