f5052eae630bb38efd6d423c2f0e0bd8879a22bf
Cristian Dinu <goc9000@gmail.com>
2017-01-03 01:42:46 +0200

    Generate deserialization operators for enums

--- build_assistant/autogen/enums/EnumCodeGenerator.py
+++ build_assistant/autogen/enums/EnumCodeGenerator.py
@@ -10,6 +10,7 @@ from build_assistant.autogen.AutoGenConfig import EnumConfig
 from build_assistant.codegen.codegen_utils import cpp_string_literal
 from build_assistant.util.Augment import Augment
 from build_assistant.util.grammar import camelcase_to_underscore, classname_to_varname
+from build_assistant.util.VirtualPath import VirtualPath
 
 
 class EnumCodeGenerator(Augment):
@@ -33,13 +34,21 @@ class EnumCodeGenerator(Augment):
         self._gen_name_for_function(cpp_source, h_source)
         h_source.code.nl()
         self._gen_debug_write_operator(cpp_source, h_source)
+        h_source.code.nl()
+        self._gen_deserialize_operator(cpp_source, h_source)
         self._gen_serialize_operator(cpp_source, h_source)
 
     def as_var_name(self):
         return classname_to_varname(self._name)
 
+    def as_ref_var_name(self):
+        return 'mut_' + classname_to_varname(self._name)
+
     def as_parameter_spec(self):
         return self._name, self.as_var_name()
+
+    def as_ref_parameter_spec(self):
+        return self._name + '&', self.as_ref_var_name()
 
     def underlying_type(self):
         return \
@@ -115,6 +124,33 @@ class EnumCodeGenerator(Augment):
         ).code_line('stream << QP({0}({1}))', self._name_for_function(), self.as_var_name()) \
             .nl() \
             .ret('stream')
+
+    def _gen_deserialize_operator(self, cpp_source, h_source):
+        cpp_source \
+            .include('utils/serialization/deserialization_helpers.h') \
+            .use(VirtualPath([cpp_source.core.codegen_cfg.base_namespace, 'utils', 'serialization']))
+
+        with cpp_source.code.function(
+            'operator>> ',
+            'QDataStream&',
+            ('QDataStream&', 'mut_stream'),
+            self.as_ref_parameter_spec(),
+            declare_in=h_source.code
+        ) as fn:
+            fn \
+                .code_line('{0} code = must_deserialize(mut_stream, {0})', self.underlying_type()) \
+                .nl()
+
+            with fn.switch_block('({0})code'.format(self._name)) as sw:
+                sw.case_block(*[self._name + '::' + value.constant for value in self.values]) \
+                    .code_line('{0} = ({1})code', self.as_ref_var_name(), self._name) \
+                    .code_line('break')
+                sw.default_block().code_line(
+                    'invariant_violation("Invalid deserialized {0} value (%lld)", (int64_t)code)', self._name
+                )
+
+            fn.nl() \
+                .ret('mut_stream')
 
     def _gen_serialize_operator(self, cpp_source, h_source):
         cpp_source.code.function(
