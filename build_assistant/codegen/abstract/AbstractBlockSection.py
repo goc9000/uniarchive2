ee4b6d7cefdd6ea8941905d0b726260e512376aa
Cristian Dinu <goc9000@gmail.com>
2017-01-31 18:18:40 +0200

    Add “content if empty” facility to code sections

--- build_assistant/codegen/abstract/AbstractBlockSection.py
+++ build_assistant/codegen/abstract/AbstractBlockSection.py
@@ -12,18 +12,30 @@ from build_assistant.codegen.abstract.AbstractCodeSectionWithContent import Abst
 
 class AbstractBlockSection(AbstractCodeSectionWithContent):
     auto_collapse = None  # Return no lines if content is empty
+    content_if_empty = None
 
     def __init__(self, source, auto_collapse=False):
         super().__init__(source)
 
+        from build_assistant.codegen.IndentedCodeSection import IndentedCodeSection
+
         self.auto_collapse = auto_collapse
+        self.content_if_empty = IndentedCodeSection(source, 1)
 
     def gen_lines(self, indent_level):
         if not self.auto_collapse:
             for line in self._gen_lines_for_items(self._gen_header_items(indent_level), indent_level):
                 yield line
+
+            is_content_empty = True
             for line in filter_lines(self._gen_lines_for_items(self._gen_items(indent_level), indent_level)):
                 yield line
+                is_content_empty = False
+
+            if is_content_empty:
+                for line in filter_lines(self.content_if_empty.gen_lines(indent_level)):
+                    yield line
+
             for line in self._gen_lines_for_items(self._gen_footer_items(indent_level), indent_level):
                 yield line
         else:
