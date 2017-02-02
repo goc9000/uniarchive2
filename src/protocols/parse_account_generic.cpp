79917ce0b02df44daf035eb64f0284acb6f57647
Cristian Dinu <goc9000@gmail.com>
2017-02-02 23:25:17 +0200

    Parse more kinds of accounts in parse_account_generic

--- src/protocols/parse_account_generic.cpp
+++ src/protocols/parse_account_generic.cpp
@@ -10,8 +10,10 @@
 
 #include "protocols/parse_account_generic.h"
 #include "protocols/digsby/digsby_account_name.h"
+#include "protocols/facebook/facebook_account_name.h"
 #include "protocols/jabber/jabber_account_name.h"
 #include "protocols/msn/msn_account_name.h"
+#include "protocols/skype/skype_account_name.h"
 #include "protocols/yahoo/yahoo_account_name.h"
 #include "protocols/IMProtocol.h"
 #include "utils/language/invariant.h"
@@ -22,15 +24,19 @@
 namespace uniarchive2 { namespace protocols {
 
 using namespace uniarchive2::protocols::digsby;
+using namespace uniarchive2::protocols::facebook;
 using namespace uniarchive2::protocols::jabber;
 using namespace uniarchive2::protocols::msn;
+using namespace uniarchive2::protocols::skype;
 using namespace uniarchive2::protocols::yahoo;
 
 FullAccountName parse_account_generic(IMProtocol protocol, IMM(QString) account_name) {
     switch (protocol) {
         case IMProtocol::DIGSBY: return parse_digsby_account(account_name);
+        case IMProtocol::FACEBOOK: return parse_facebook_account(account_name);
         case IMProtocol::JABBER: return parse_jabber_account(account_name);
         case IMProtocol::MSN: return parse_msn_account(account_name);
+        case IMProtocol::SKYPE: return parse_skype_account(account_name);
         case IMProtocol::YAHOO: return parse_yahoo_account(account_name);
         default:
             invariant_violation("Unsupported protocol for parsing an account: %s", QP(name_for_im_protocol(protocol)));
