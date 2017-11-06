9df503fca849443d4ba94741eac6c523e3652f32
Cristian Dinu <goc9000@gmail.com>
2017-11-07 00:43:11 +0200

    Show a debug printout for unresolved subjects

--- src/fixers/resolve_subjects/resolve_subjects.cpp
+++ src/fixers/resolve_subjects/resolve_subjects.cpp
@@ -18,9 +18,14 @@
 #include "utils/qt/shortcuts.h"
 #include "utils/qt/debug_extras.h"
 
+#include <map>
+#include <vector>
+#include <algorithm>
+
 namespace uniarchive2 { namespace fixers { namespace resolve_subjects {
 
 using namespace intermediate_format::subjects;
+using namespace std;
 
 class ResolveSubjectsProcess : IApparentSubjectVisitor {
 public:
@@ -29,14 +34,18 @@ public:
 
     void run(RawConversationCollection& mut_conversations) {
         mut_conversations.visitSubjects(*this);
+        finish();
     }
 
     void run(RawConversation& mut_conversation) {
         mut_conversation.visitSubjects(*this);
+        finish();
     }
 
 protected:
     IMM(ResolveSubjectsConfig) config;
+
+    map<QString, int> debugUnresolvedSubjects;
 
     bool visit(unique_ptr<ApparentSubject>& mut_subject) {
         invariant(!!mut_subject, "Did not expect unset subject!");
@@ -47,7 +56,8 @@ protected:
             return true; // Leave implicit and already resolved subjects alone
         }
 
-        tryExactAccountMatch(mut_subject);
+        tryExactAccountMatch(mut_subject) ||
+            unresolvedSubject(mut_subject);
 
         return true;
     }
@@ -82,6 +92,56 @@ protected:
         mut_subject = make_unique<ResolvedSubject>(resolved_subject_id, move(mut_subject), hints);
 
         return true;
+    }
+
+    bool unresolvedSubject(IMM(unique_ptr<ApparentSubject>) subject) {
+        QString repr;
+        QDebug(&repr) << subject;
+
+        repr = repr.replace(" [peer]", "").replace(" [ident]", "").trimmed();
+
+        debugUnresolvedSubjects[repr]++;
+
+        return true;
+    }
+
+    void finish() {
+        // TODO: remove temporary debug printout
+        dumpUnresolvedSubjects();
+    }
+
+    void dumpUnresolvedSubjects() {
+        if (debugUnresolvedSubjects.empty()) {
+            return;
+        }
+
+        vector<pair<QString, int>> results;
+        int total_occurrences = 0;
+
+        for (IMM(auto) kv : debugUnresolvedSubjects) {
+            results.push_back(kv);
+            total_occurrences += kv.second;
+        }
+
+        sort(results.begin(), results.end(), [](auto &a, auto &b) { return a.second > b.second; });
+
+        QString summary =
+            QString("%1 unresolved subjects, %2 total occcurences").arg(results.size()).arg(total_occurrences);
+        qDebug() << QP(summary);
+        qDebug() << "=================================================================";
+
+        int lines_printed = 0;
+        int MAX_LINES = 200;
+
+        for (IMM(auto) kv : results) {
+            qDebug() << QP(QString("%1 x %2").arg(kv.second, 5).arg(kv.first));
+
+            lines_printed++;
+            if (lines_printed >= MAX_LINES) {
+                qDebug() << QP(QString("...(%1 more lines)...").arg(results.size() - lines_printed));
+                break;
+            }
+        }
     }
 };
 
